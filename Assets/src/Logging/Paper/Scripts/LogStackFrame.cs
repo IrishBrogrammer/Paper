9a6587889f5bd88b1d9abb44a18a34a6bf0f08f3
Brian McAleese <bmcaleese12@gmail.com>
2016-03-17 16:02:31 +0000

    Add a class to contain info on stack frame of log

---
+++ Assets/src/Logging/Paper/Scripts/LogStackFrame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+using System.Collections;
+
+public class LogStackFrame 
+{
+	public string MethodName;
+	public string DeclaringType;
+	public string ParameterSig;
+
+	public int LineNumber;
+	public string Filename;
+
+	public LogStackFrame(StackFrame frame)
+	{
+		var Method = frame.GetMethod();
+		MethodName = Method.Name;
+		DeclaringType = Method.DeclaringType.Name;
+
+		var param = Method.GetParameters();
+
+		for (int i = 0; i < param.Length; i++)
+		{
+			ParameterSig += string.Format("{0} {1}", param[i].ParameterType, param[i].Name);
+
+			if (i + 1 < param.Length)
+				ParameterSig += " , ";
+		}
+
+		Filename = frame.GetFileName();
+		LineNumber = frame.GetFileLineNumber();
+	}
+
+	public override string ToString()
+	{
+		return Filename + " : " + LineNumber + " : " + MethodName;
+	}
+
+
+}
