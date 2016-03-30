491ac96a6d1fd872381a7b4b92b67be6d8f57262
Brian McAleese <bmcaleese12@gmail.com>
2016-03-30 17:00:22 +0100

    Better formating of stack trace, includes method name and signature in output

--- Assets/src/Logging/Paper/Scripts/LogStackFrame.cs
+++ Assets/src/Logging/Paper/Scripts/LogStackFrame.cs
@@ -14,8 +14,9 @@ public class LogStackFrame
 	public string ParameterSig;
 
 	public int LineNumber;
+	public int ColumnNumber;
 	public string Filename;
-
+	
 	public LogStackFrame(StackFrame frame)
 	{
 		var Method = frame.GetMethod();
@@ -26,19 +27,21 @@ public class LogStackFrame
 
 		for (int i = 0; i < param.Length; i++)
 		{
-			ParameterSig += string.Format("{0} {1}", param[i].ParameterType, param[i].Name);
+			ParameterSig += string.Format("{0} {1}", param[i].ParameterType.Name, param[i].Name);
 
 			if (i + 1 < param.Length)
 				ParameterSig += " , ";
 		}
 
+		
 		Filename = frame.GetFileName();
 		LineNumber = frame.GetFileLineNumber();
+		ColumnNumber = frame.GetFileColumnNumber();
 	}
 
 	public override string ToString()
 	{
-		return Filename + " : " + LineNumber + " : " + MethodName;
+		return DeclaringType + ":" + MethodName + "( " + ParameterSig  + ")"  + " at  " + " ( " + Filename + " : " + LineNumber + " ) ";
 	}
 
 
